8388ae55dbdcd529e768859d8dd1111b2add925c
Prashant Kali <you@example.com>
2025-08-04 22:33:06 +0530

    Refactor JavaScript event listeners and enhance CSS layout for improved color palette functionality

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 let gBotton = document.querySelector(".btn");
-gBotton.addEventListener("click", getColors);
 let btncolor = document.querySelectorAll(".selectcolor");
 let bgcolor = document.querySelectorAll(".color");
+let copyicon = document.querySelectorAll(".fa-copy");
+
+gBotton.addEventListener("click", getColors);
+copyicon.forEach(function (icon) {
+  icon.addEventListener("click", function (e) {
+    if (e.target.classList.contains("fa-copy")) {
+      const hexVal = e.target.previousElementSibling.textContent;
+      navigator.clipboard.writeText(hexVal);
+    }
+  });
+});
 
 function getColors() {
   let color = "ABCDEF1234567890";
@@ -11,7 +21,9 @@ function getColors() {
       let rNum = Math.floor(Math.random() * 16);
       num = num + color[rNum];
     }
+    copyicon[idx].previousElementSibling.textContent = num;
     btn.style.backgroundColor = num;
+
     num = "#";
   });
 }
